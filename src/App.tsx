0881c898ae5373e88083c1e040543852894b6b90
julia-hebel <j.hebel077@gmail.com>
2022-10-18 14:52:50 +0200

    added routing and created base pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className='bg-zinc-800 min-h-screen text-white'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
