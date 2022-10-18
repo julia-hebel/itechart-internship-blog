6bf16b35cc9282489aa9a3463f1c46a382a90e00
julia-hebel <j.hebel077@gmail.com>
2022-10-18 18:05:00 +0200

    implemented store, created postSlice and post component

---
+++ src/components/Post.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Post({ post }: any) {
+  return <div>Post</div>;
+}
+
+export default Post;
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from '../redux/postsSlice';
+import Post from './Post';
 
 function PostsList() {
-  return (
-    <div>PostsList</div>
-  )
+  const dispatch = useDispatch<any>();
+  const posts = useSelector((state: any) => state.posts.posts);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  console.log('posts', posts);
+  const date = new Date().toISOString();
+  console.log(date);
+
+  return <div>{posts.map((post: any) => {
+    return (
+        <Post key={post.id} post={post} />
+    )
+  })}</div>;
 }
 
-export default PostsList
+export default PostsList;
