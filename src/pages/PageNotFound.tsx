e87336571f4c76142d20b23e0d3d588b6f8809a4
julia-hebel <j.hebel077@gmail.com>
2022-10-20 17:05:09 +0200

    implemented page not found

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-8xl my-4'>404</h1>
+      <div className='text-3xl my-4'>Page Not Found</div>
+      <div className='my-4'>The resource could not be found on this server</div>
+      <Link
+        to='/'
+        className='my-4 border border-white rounded-lg bg-[rgb(117,11,150)] p-2'
+      >
+        Take me home
+      </Link>
+    </div>
   )
 }
 
